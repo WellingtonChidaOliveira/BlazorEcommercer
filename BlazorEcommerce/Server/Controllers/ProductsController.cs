c073de17683b9ea9a0f2d52ad7996d489f89b937
Wchida <wellingtonchida@protonmail.com>
2023-05-01 10:00:53 -0300

    ♻️ refactor: adjust de controller product to usa data in data base

--- BlazorEcommerce/Server/Controllers/ProductsController.cs
+++ BlazorEcommerce/Server/Controllers/ProductsController.cs
@@ -1,6 +1,4 @@
-﻿using BlazorEcommerce.Server.Data;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers
 {
@@ -9,79 +7,59 @@ namespace BlazorEcommerce.Server.Controllers
 
     public class ProductsController : ControllerBase
     {
-        //private List<Product> products = new();
         private DataContext _service;
-        public ProductsController( DataContext service)
+        public ProductsController(DataContext service)
         {
-            _service= service;
-            //MockList();
+            _service = service;
+
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> Get()
         {
-           var products =  await _service.Products.FindAsync();
+            var products = await _service.Products.ToListAsync();
             return Ok(products);
         }
 
         [HttpPost]
-        public async Task Post(Product product)
+        public async Task<ActionResult> Post(Product product)
         {
             await _service.Products.AddAsync(product);
             await _service.SaveChangesAsync();
+            return NoContent();
         }
         [HttpPost("/update")]
-        public void Update(Product product)
+        public async Task<ActionResult> Update(Product product)
         {
-            var productToUpdate = _service.Products.FirstOrDefault(p => p.Id == product.Id);
-            if(productToUpdate != null)
+            var productToUpdate = await _service.Products.FirstOrDefaultAsync(p => p.Id == product.Id);
+            if (productToUpdate is null)
+            {
+                return NotFound();
+            }
+            else
             {
                 productToUpdate.Title = product.Title;
                 productToUpdate.Description = product.Description;
                 productToUpdate.ImageUrl = product.ImageUrl;
                 productToUpdate.Price = product.Price;
             }
+            await _service.SaveChangesAsync();
+            return NoContent();
+
         }
 
         [HttpDelete]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            var product = _service.Products.FirstOrDefault(p => p.Id == id);
-            if(product != null) _service.Products.Remove(product);
+            var product = await _service.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (product is null) return NotFound();
+
+            _service.Products.Remove(product);
+
+            return NoContent();
         }
 
-        //[HttpGet("mocklist")]
-        //public void MockList()
-        //{
 
-        //    products = new List<Product> {
-        //    new Product
-        //    {
-        //        Id = 1,
-        //        Title = "The Hitchhiker's Guide to the Galaxy",
-        //        Description = "The Hitchhiker's Guide to the Galaxy[a][b] is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including novels, stage shows, comic books, a 1981 TV series, a 1984 text-based computer game, and 2005 feature film.",
-        //        ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
-        //        Price = 199.99m
-        //    },
-        //     new Product
-        //     {
-        //         Id = 2,
-        //         Title = "Ready Player One",
-        //         Description = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopia in 2045, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which would lead him to inherit the game creator's fortune. Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House).[1] The book was published on August 16, 2011.[2] An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.[3][4]Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association[5] and won the 2011 Prometheus Award.[6]\r\n\r\n",
-        //         ImageUrl = "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
-        //         Price = 299.99m
-        //     },
-
-        //    new Product
-        //    {
-        //        Id = 3,
-        //        Title = "Nineteen Eighty-Four",
-        //        Description = "Nineteen Eighty-Four (also published as 1984) is a dystopian social science fiction novel and cautionary tale by English writer George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime. Thematically, it centres on the consequences of totalitarianism, mass surveillance and repressive regimentation of people and behaviours within society.[2][3] Orwell, a democratic socialist, modelled the authoritarian state in the novel on Stalinist Russia and Nazi Germany.[2][3][4] More broadly, the novel examines the role of truth and facts within societies and the ways in which they can be manipulated.",
-        //        ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
-        //        Price = 399.99m
-        //    }
-        //};
-        //}
 
     }
 
