c6b979592bd5e546504ea87395b587bfcce44c5d
Wchida <wellingtonchida@protonmail.com>
2023-05-01 19:35:34 -0300

    ✨ feat: add category service and controller to get all category

---
+++ BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorEcommerce.Server.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        public DataContext _service;
+        public CategoryService(DataContext service)
+        {
+            _service = service;
+        }
+
+       
+
+        public async Task<ServiceResponse<List<Category>>> GetCategories()
+        {
+            var categories = await _service.Categories.ToListAsync();
+            return new ServiceResponse<List<Category>>
+            {
+                Data = categories
+            };
+        }
+    }
+}
