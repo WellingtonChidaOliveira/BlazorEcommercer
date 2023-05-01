08b7d2db085e127f687c669d0c74c779b20b28b3
Wchida <wellingtonchida@protonmail.com>
2023-05-01 01:50:49 -0300

    ✨ feat: add ef core and migration to data base

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
