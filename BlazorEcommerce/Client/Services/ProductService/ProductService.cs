83c4d9111a82363f9d53922f9f7c5fd6eca4c974
Wchida <wellingtonchida@protonmail.com>
2023-05-01 12:00:37 -0300

    ♻️ refactor: add service to client side

---
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace BlazorEcommerce.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        public HttpClient _httpClient { get; }
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+
+        public async Task GetProducts()
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/products");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+
+        }
+    }
+}
--- BlazorEcommerce/Client/Shared/ProductList.razor.cs
+++ BlazorEcommerce/Client/Shared/ProductList.razor.cs
@@ -1,19 +1,11 @@
-﻿using BlazorEcommerce.Shared;
-using System.Net.Http.Json;
-
-namespace BlazorEcommerce.Client.Shared
+﻿namespace BlazorEcommerce.Client.Shared
 {
     public partial class ProductList
     {
-        private List<Product> Products = new List<Product>();
 
         protected override async Task OnInitializedAsync()
         {
-            var result = await Http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Products");
-            if(result != null && result.Data != null)
-            {
-                Products = result.Data;
-            }
+           await ProductService.GetProducts();
         }
     }
 }
